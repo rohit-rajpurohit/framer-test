da8b34cc6bd617e9e77c3feea343dc3033e2d40b
Rohit-Rajpurohit <rajpurohitrohit869@gmail.com>
2022-02-14 23:54:29 +0530

    add files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Animation from "./animation";
+
+function App() {
+  return (
+    <div className="App">
+      <Animation />
+    </div>
+  );
+}
+
+export default App;
